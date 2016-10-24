ae6e973c44a65eded16a83afdc9589ae90e56ff1
David Hackro <hackro91@gmail.com>
2016-10-24 17:38:46 -0500

    version 1.6

--- app/src/main/java/pue/mtz/pau/appquimica/Views/MainActivityImpl.java
+++ app/src/main/java/pue/mtz/pau/appquimica/Views/MainActivityImpl.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -108,7 +109,7 @@ public class MainActivityImpl extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
