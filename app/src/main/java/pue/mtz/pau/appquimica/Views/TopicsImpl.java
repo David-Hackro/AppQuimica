fb7c8bd10d849ac5e9bb3b56a9b9ae70fff95bbb
David Hackro <hackro91@gmail.com>
2016-10-24 16:26:26 -0500

    version 1.4

--- app/src/main/java/pue/mtz/pau/appquimica/Views/TopicsImpl.java
+++ app/src/main/java/pue/mtz/pau/appquimica/Views/TopicsImpl.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.ProgressBar;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,57 +13,54 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import pue.mtz.pau.appquimica.Adapters.TopicsAdapter;
+import pue.mtz.pau.appquimica.Interfaces.Presenters.TopicsPresenter;
 import pue.mtz.pau.appquimica.Models.Ingredient;
 import pue.mtz.pau.appquimica.ExpandibleList.Recipe;
 import pue.mtz.pau.appquimica.Interfaces.Views.TopicsView;
 import pue.mtz.pau.appquimica.Models.Topic;
 import pue.mtz.pau.appquimica.Models.Topics;
+import pue.mtz.pau.appquimica.Presenters.TopicsPresenterImpl;
 import pue.mtz.pau.appquimica.R;
 
 public class TopicsImpl extends AppCompatActivity implements TopicsView{
 
-    /*@BindView(R.id.progress)
+    @BindView(R.id.progress)
     ProgressBar progressBar;
-    @BindView(R.id.RecyclerView)
-    RecyclerView recyclerView;
 
-    private TopicsAdapter adapter;
-    private TopicsPresenter presenter;
-*/
+
+    public TopicsPresenter presenter;
+
     @BindView(R.id.RecyclerView)
     public RecyclerView mRecyclerView;
+
     private TopicsAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topics);
         ButterKnife.bind(this);
 
-  /*      ButterKnife.bind(this);
-
         presenter = new TopicsPresenterImpl(this);
-
-        recyclerView.setHasFixedSize(true);
-        //recyclerView = (RecyclerView) findViewById(R.id.RecyclerView);
-        adapter = new TopicsAdapter(R.layout.card_topic);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setHasFixedSize(true);
-        LinearLayoutManager llm = new LinearLayoutManager(this.getApplicationContext());
-        llm.setOrientation(LinearLayoutManager.VERTICAL);
-        recyclerView.setLayoutManager(llm);
 
         presenter.loadListPerson();
 
     }
 
-    @Override
-    public void initRecycler(List<Topic> topics) {
-        adapter.setListTopics(topics);
 
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        adapter.onSaveInstanceState(savedInstanceState);
     }
 
-}
-*/
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        adapter.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void initRecycler(Topics persons) {
         //String name, String icon, String description
         Topic field1 = new Topic("calculo","calculo");
         Topic field2 = new Topic("aritmetica","aritmetica");
@@ -104,24 +102,6 @@ public class TopicsImpl extends AppCompatActivity implements TopicsView{
         });
 */
         mRecyclerView.setAdapter(adapter);
-
-
-    }
-
-
-    protected void onSaveInstanceState(Bundle savedInstanceState) {
-        super.onSaveInstanceState(savedInstanceState);
-        adapter.onSaveInstanceState(savedInstanceState);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        adapter.onRestoreInstanceState(savedInstanceState);
-    }
-
-    @Override
-    public void initRecycler(Topics persons) {
 
     }
 }
